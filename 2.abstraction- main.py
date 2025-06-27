0f7aac6daf69519f6332b8f5b2c0b5259cd1d830
Sandhiya <sandhiyaravichandran2206@gmail.com>
2025-06-27 17:58:18 +0530

    Update 2.abstraction- main.py

--- 2.abstraction- main.py
+++ 2.abstraction- main.py
@@ -1,6 +1,6 @@
 from item import Item
 item1 = Item("xty",700,1)
 item1.sendEmail()
-
-Abstraction;Abstraction is the concept of hiding the internal implementation and showing only the necessary details to the user.
-here we can abstract  we cant see the process how the mail sent.i.e the other functions called in sendEmail()beacuse that is private we cant access or see it outside the class.i hide it by making it private inside the class
+item1.__connect()#we cant access/see it outside the class,because it is private in the class.
+Abstraction;Abstraction is the concept of hiding the internal implementation and showing/acess only the necessary details to the user.
+here the only necessary method sendEmail()is shown /exposed to user and others are hidden by making it private.
