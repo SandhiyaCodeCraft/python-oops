382900eacc5ede633ba4a1dae8f723c13b481068
Sandhiya <sandhiyaravichandran2206@gmail.com>
2025-06-27 17:43:16 +0530

    Create 1.Abstraction- items.py

---
+++ 1.Abstraction- items.py
@@ -0,0 +1,21 @@
+Class Item:
+              "
+              "
+          @staticmethod
+
+    def__connect(self, smpt_server):
+                 pass
+
+    def__prepare_body(self):
+                return f""
+                      Hello Someone.
+                      We have (self.name} {self.quantity times.
+                      Regards, JisshapedCoding
+                      ""
+    def __send(self):
+                pass
+
+    def send_email(self):
+            self.__connect
+            self.__prepare_body()
+            self.__send()
